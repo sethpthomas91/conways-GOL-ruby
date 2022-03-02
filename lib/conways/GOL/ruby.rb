fe258ab7dcb74ba88c30d63b071e2d23c12578af
Seth Thomas <sthomas@seths-mbp-2.lan>
2022-03-02 17:24:57 -0500

    First commit

---
+++ lib/conways/GOL/ruby.rb
@@ -0,0 +1,10 @@
+require "conways/GOL/ruby/version"
+
+module Conways
+  module GOL
+    module Ruby
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/conways/GOL/ruby/version.rb
@@ -0,0 +1,7 @@
+module Conways
+  module GOL
+    module Ruby
+      VERSION = "0.1.0"
+    end
+  end
+end
