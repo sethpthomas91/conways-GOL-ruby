e4facb290e2fac4dc3e0df798df78c08a1868887
Scott Plunkett <splunkett@8thLight.com>
2022-03-04 17:00:29 -0600

    [REFACTOR] Remove extraneous files + classses & rewrite specs.

--- lib/world.rb
+++ lib/world.rb
@@ -1,29 +1,52 @@
-require "cell"
-
 class World
-    attr_reader :cell_grid
+  attr_reader :cells
 
-    def initialize
-        @cell_grid = create_empty_world
+  DEAD = 0
+  ALIVE = 1
+
+  def initialize(cells)
+    @cells = cells
+  end
+
+  def tick
+    next_generation = []
+
+    @cells.count.times do
+      next_generation << []
     end
 
-    def create_empty_world
-        new_cell_grid = []
-        width = 10
-        while new_cell_grid.length() < width**2
-            new_cell_grid << Cell.new
-        end
-        @cell_grid = new_cell_grid
+    @cells.each_with_index do |cell_row, row|
+      cell_row.each_with_index do |cell, column|
+        neighbors = neighbors(row, column)
+        living_neighbors = neighbors.count { |neighbor| neighbor == ALIVE }
+        next_generation[row] << next_state(living_neighbors)
+      end
     end
+    @cells = next_generation
+  end
 
-    def is_empty
-        for cell in @cell_grid do
-            if cell.is_alive === true
-                return false
-            end
-        end
-        return true
-    end
+  private
 
+  def next_state(living_neighbors)
+    (living_neighbors == 2 || living_neighbors == 3) ? ALIVE : DEAD
+  end
 
+  def neighbors(row, column)
+    max = @cells.length - 1
+    next_row = row >= max ? 0 : (row + 1)
+    next_col = column >= max ? 0 : (column + 1)
+    [
+      # above
+      @cells[row - 1][column - 1],
+      @cells[row - 1][column],
+      @cells[row - 1][next_col],
+      # in row
+      @cells[row][column - 1],
+      @cells[row][next_col],
+      # below
+      @cells[next_row][column - 1],
+      @cells[next_row][column],
+      @cells[next_row][next_col]
+    ]
+  end
 end
--- spec/world_spec.rb
+++ spec/world_spec.rb
@@ -1,10 +1,56 @@
 require 'world'
 
-RSpec.describe World, "#cell_grid"  do
-    context "upon creation of the world" do
-        it "indicates that there are no living cells" do
-            world = World.new
-            expect(world.is_empty).to eq(true)
-        end
+RSpec.describe World do
+  DEAD = World::DEAD
+  ALIVE = World::ALIVE
+
+  describe '#tick' do
+    it 'any live cell with less than two live neighbors dies' do
+      initial_cells = [
+        [DEAD, DEAD, DEAD],
+        [DEAD, ALIVE, DEAD],
+        [DEAD, DEAD, DEAD]
+      ]
+
+      world = World.new(initial_cells)
+      world.tick
+      expect(world.cells[1][1]).to eq(DEAD)
     end
+
+    it 'any live cell with 2 or 3 live neighbors lives' do
+      initial_cells = [
+        [DEAD, DEAD, DEAD],
+        [ALIVE, ALIVE, ALIVE],
+        [DEAD, DEAD, DEAD]
+      ]
+
+      world = World.new(initial_cells)
+      world.tick
+      expect(world.cells[1][1]).to eq(ALIVE)
+    end
+
+    it 'any live cell with more than 3 live neighbors dies' do
+      initial_cells = [
+        [ALIVE, DEAD, DEAD],
+        [ALIVE, ALIVE, DEAD],
+        [ALIVE, ALIVE, DEAD]
+      ]
+
+      world = World.new(initial_cells)
+      world.tick
+      expect(world.cells[1][1]).to eq(DEAD)
+    end
+
+    it 'any dead cell with exactly 3 live neighbors lives' do
+      initial_cells = [
+        [ALIVE, ALIVE, DEAD],
+        [ALIVE, DEAD, DEAD],
+        [DEAD, DEAD, DEAD]
+      ]
+
+      world = World.new(initial_cells)
+      world.tick
+      expect(world.cells[1][1]).to eq(ALIVE)
+    end
+  end
 end
